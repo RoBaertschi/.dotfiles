58bc637ba64d34a91cf66ec83dbc250920d4f91d
Robin Bärtschi <robin.baertschi@swisscom.com>
2024-05-01 10:48:59 +0200

    changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,21 @@
 
+# Setup
+
+function v1
+  fisher install jorgebucaran/fisher
+  fisher install jorgebucaran/nvm.fish
+  fisher install reitzig/sdkman-for-fish@v2.0.0
+  fisher install budimanjojo/tmux.fish
+  fisher install edc/bass
+
+  curl https://raw.githubusercontent.com/tristanisham/zvm/master/install.sh | bash
+
+  set -U fish_personal_setup 1
+
+  set -U nvm_default_version latest
+end
+
+
 alias emsdk_setup ". /home/robin/aur/emsdk/emsdk_env.fish"
 
 set -gx GBDKDIR ~/aur/gbdk
@@ -15,11 +32,30 @@ fish_add_path ~/aur/sdcc/bin
 fish_add_path ~/aur/gbdk/bin
 fish_add_path ~/aur/Emulicious
 fish_add_path /home/linuxbrew/.linuxbrew/bin
+set -gx ZVM_INSTALL "$HOME/.zvm/self"
+fish_add_path $ZVM_INSTALL
+fish_add_path $HOME/.zvm/bin
 if status is-interactive
+  
+
     # Commands to run in interactive sessions can go here
     alias vim='NVIM_APPNAME="nvim-kickstart" nvim'
     alias kvim='NVIM_APPNAME="nvim-kickstart" nvim'
     starship init fish | source
+
+    fzf --fish | source
+
+  if not set -q fish_personal_setup
+    echo "No fish_personal_setup"
+    v1
+  else if [ $fish_personal_setup != "1" ] 
+    echo "Outdated fish setup version"
+    v1
+  end
+
+  if set -q work_laptop
+    bass source ~/.wsl4sc
+  end
 end
 
 zoxide init fish --cmd=cd | source
